f363c44ca42fb055f1ae6ff1241ba12f61fa66b1
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-06 09:27:30 +0700

    add app.js to index.js

---
+++ user/assets/scripts/index.js
@@ -0,0 +1,6 @@
+import App from './views/app.js';
+
+const app = new App({
+  buttonUserProfile: document.querySelector('user-profile__info'),
+  drawerUserProfile: document.querySelector('user-profile__dropdown'),
+});
