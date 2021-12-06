e7a705f20ba489a1ea74754573c9bcfdd53e37b8
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-07 04:22:39 +0700

    add new argument to App initialization to incorporate navbar drawer function

--- user/assets/scripts/index.js
+++ user/assets/scripts/index.js
@@ -3,4 +3,6 @@ import App from './views/App.js';
 const app = new App({
   buttonUserProfile: document.querySelector('.user-profile__info'),
   drawerUserProfile: document.querySelector('.user-profile__dropdown'),
+  buttonNavbar: document.querySelector('.app-bar_menu-hamburger'),
+  drawerNavbar: document.querySelector('.sidenav'),
 });
