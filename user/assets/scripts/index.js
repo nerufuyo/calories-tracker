f44a3d8893273b69156faa2b44463bd8d37a05bc
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-06 09:53:07 +0700

    fixing and change App initialization

--- user/assets/scripts/index.js
+++ user/assets/scripts/index.js
@@ -1,6 +1,6 @@
 import App from './views/App.js';
 
-const app = new App({
-  buttonUserProfile: document.querySelector('user-profile__info'),
-  drawerUserProfile: document.querySelector('user-profile__dropdown'),
+const App = new App({
+  buttonUserProfile: document.querySelector('.user-profile__info'),
+  drawerUserProfile: document.querySelector('.user-profile__dropdown'),
 });
