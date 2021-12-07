7ca9cfd9294256c6114107ce5bdf6ee3d2422184
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-07 12:42:43 +0700

    change setCooke() parameter to object

--- user/assets/scripts/utils/CookieHelper.js
+++ user/assets/scripts/utils/CookieHelper.js
@@ -1,5 +1,5 @@
 class CookieHelper {
-  static setCookie(cname, cvalue, exdays) {
+  static setCookie({ cname, cvalue, exdays }) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
