84e8eb131e204e2fa8ed5adf6e8bf39bde5b2c22
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-09 09:39:41 +0700

    add getById, getByDate, getByMonth function to FoodDiaryData

--- user/assets/scripts/data/FoodDiaryData.js
+++ user/assets/scripts/data/FoodDiaryData.js
@@ -1,5 +1,11 @@
 import foodDiaryDummy from "./foodDiaryDummy.js";
 
+function compareMonth(fullDate, month) {
+  const fullDateToMonth = fullDate.slice(0,7);
+
+  return fullDateToMonth === month;
+}
+
 const FoodDiaryData = {
   async getById(id) {
     if (!id) {
@@ -7,6 +13,14 @@ const FoodDiaryData = {
     }
 
     return foodDiaryDummy.find(food => food.id === id);
+  },
+
+  async getByDate(date) {
+    return foodDiaryDummy.filter(food => food.date === date);
+  },
+
+  async getByMonth(month) {
+    return foodDiaryDummy.filter(food => compareMonth(food.date, month));
   }
 }
 
