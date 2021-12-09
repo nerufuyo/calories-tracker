587aea66b30224c6ef37444f27b888688da70c62
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-09 22:11:20 +0700

    change let date to const date

--- user/assets/scripts/utils/getMonthName.js
+++ user/assets/scripts/utils/getMonthName.js
@@ -1,5 +1,5 @@
 function getMonthName(dateInput) {
-  let date = dateInput ? new Date(dateInput) : new Date();
+  const date = dateInput ? new Date(dateInput) : new Date();
   
   return date.toLocaleString('default', { month: 'long' });
 }
