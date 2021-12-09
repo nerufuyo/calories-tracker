5d12d9c9bc06c1c852a80ee322d6a28803a1be0a
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-09 11:16:42 +0700

    add DashboardView

---
+++ user/assets/scripts/views/pages/dashboard/DashboardView.js
@@ -0,0 +1,40 @@
+class DashboardView {
+  getHeadTemplate() {
+    return `
+      <i class="fas fa-chart-pie"></i> Dashboard
+    `;
+  }
+
+  getContentTemplate() {
+    return `
+      <div class="dashboard-top">
+        <div class="month-picker">
+          <div class="previous-month"></div>
+          <div class="next-month"></div>
+          <div class="selected-month"></div>
+        </div>
+        <div class="bmi">
+          Your BMI: <span class="bmi-data"></span>
+        </div>
+        <div class="calories-goal">
+          Goal: <span class="calories-goal-data"></span>
+        <div>
+      </div>
+      <div class="calendar">
+        <div class="calendar-days">
+          <div>Sunday</div>
+          <div>Monday</div>
+          <div>Tuesday</div>
+          <div>Wednestday</div>
+          <div>Thursday</div>
+          <div>Friday</div>
+          <div>Saturday</div>
+        </div>
+        <div class="calendar-date">
+        <div></div>
+      </div>
+    `;
+  };
+}
+
+export default DashboardView;
