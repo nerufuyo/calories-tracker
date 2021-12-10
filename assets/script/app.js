3f679d5a63c5536fe15d5ed29c51dabc70e9e523
Listyo Adi Pamungkas <listyoadipamungkas1401@gmail.com>
2021-12-10 08:06:53 +0700

    Create CRUD on Profile and Log Out Account

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,24 +1,25 @@
 //   Preloader
-const preloader = document.getElementById('preloader');
-const preloaderImage = document.querySelector('.preloader-image');
-const preloaderText = document.querySelector('.preloader-text');
+// const preloader = document.getElementById('preloader');
+// const preloaderImage = document.querySelector('.preloader-image');
+// const preloaderText = document.querySelector('.preloader-text');
 
-document.documentElement.addEventListener('load', init()) 
-async function init() {
-    document.body.classList.add('stop_scrolling');
-    await sleep(4500);
-    preloader.style.display = 'none';
-    document.body.classList.remove('stop_scrolling');
-};
+// document.documentElement.addEventListener('load', init()) 
+// async function init() {
+//     document.body.classList.add('stop_scrolling');
+//     await sleep(4500);
+//     preloader.style.display = 'none';
+//     document.body.classList.remove('stop_scrolling');
+// };
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// };
 
 // Hamburger Menu 
-const hamburger = document.querySelector('nav ul.hamburger-lines');
-const hamburgerItem = document.querySelector('nav ul.navigation-list');
+const hamburger = document.querySelector('nav ul.hamburger-lines') || null;
+const hamburgerItem = document.querySelector('nav ul.navigation-list') || null;
 
+if(hamburger !== null){
 hamburger.addEventListener('click', hamburgerSlider);
 function hamburgerSlider() {
     if (hamburgerItem.style.left === '100%') {
@@ -28,4 +29,29 @@ function hamburgerSlider() {
         hamburgerItem.style.left = '100%';
         document.body.classList.remove('stop_scrolling');
     }
+    }
+}
+
+// Auth Page 
+const dontHaveAccountButton = document.getElementById('dont-have-account') || null
+const haveAccountButton = document.getElementById('have-account') || null
+
+if (dontHaveAccountButton !== null) {
+    dontHaveAccountButton.addEventListener('click', (e)=> {
+        const loginForm = document.querySelector('section.login-page div.form-collection div.login-form')
+        const registerForm = document.querySelector('section.login-page div.form-collection div.signup-form')
+
+        loginForm.style.display = 'none'
+        registerForm.style.display = 'flex'
+    })
+}
+
+if (haveAccountButton !== null) {
+    haveAccountButton.addEventListener('click', (e)=> {
+        const loginForm = document.querySelector('section.login-page div.form-collection div.login-form')
+        const registerForm = document.querySelector('section.login-page div.form-collection div.signup-form')
+
+        loginForm.style.display = 'flex'
+        registerForm.style.display = 'none'
+    })
 }
