0f1e28b8193c24ffcadbd37efd53ae55bbbc195f
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-10 12:48:43 +0700

    delete getMonthName

--- user/assets/scripts/utils/getMonthName.js
+++
@@ -1,7 +0,0 @@
-function getMonthName(dateInput) {
-  const date = dateInput ? new Date(dateInput) : new Date();
-  
-  return date.toLocaleString('default', { month: 'long' });
-}
-
-export default getMonthName;
