f65920af5f015ca086a1d3e634123e78703d0f20
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-13 07:39:15 +0700

    add FoodDiary page to router

--- user/assets/scripts/routes/routes.js
+++ user/assets/scripts/routes/routes.js
@@ -1,8 +1,10 @@
 import Dashboard from "../views/pages/dashboard.js";
+import FoodDiary from "../views/pages/FoodDiary.js";
 
 const routes = {
   '/': Dashboard,
   '/dashboard': Dashboard,
+  '/food-diary': FoodDiary,
 }
 
 export default routes;
