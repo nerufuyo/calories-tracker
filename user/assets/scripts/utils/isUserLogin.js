148d598843106cc136bdda4bcf467872920f19da
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-17 05:11:11 +0700

    check if user has logged or not (for testing yet)

---
+++ user/assets/scripts/utils/isUserLogin.js
@@ -0,0 +1,14 @@
+import {auth} from '../globals/firebaseInit.js';
+import {onAuthStateChanged} from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js';
+
+async function isUserLogin() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('user status: ', user);
+    } else {
+      console.log('user is not logged');
+    }
+  });
+}
+
+export default isUserLogin;
