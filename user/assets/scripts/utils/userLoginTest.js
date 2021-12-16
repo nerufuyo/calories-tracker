38ee048911348ee78f998f6968d153e2c94e5d3c
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-17 05:10:29 +0700

    for simulate when user login

--- user/assets/scripts/utils/userLoginTest.js
+++ user/assets/scripts/utils/userLoginTest.js
@@ -1,10 +1,5 @@
-import { firebaseConfig } from '../../../../assets/script/globals/config.js';
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js';
-import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js';
-
-
-initializeApp(firebaseConfig);
-const auth = getAuth();
+import {signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js';
+import {auth} from '../globals/firebaseInit.js';
 
 function userLoginTest() {
   signInWithEmailAndPassword(auth, 'idwimeianto@gmail.com', 'RahasiaDong');
