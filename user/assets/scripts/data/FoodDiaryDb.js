f60121699ec5835ded35f4dc56aaed44a997283e
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-16 09:53:49 +0700

    add foodDiaryDb.js

---
+++ user/assets/scripts/data/FoodDiaryDb.js
@@ -0,0 +1,21 @@
+// const FoodDiaryDb = {
+//   async addFood() {
+//     onAuthStateChanged(auth, (user) => {
+//       thingsRef.add({
+//         uid: user.uid,
+//         name: faker.commerce.productName(),
+//         createdAt: serverTimestamp()
+//       });
+//     })
+//   }
+
+//   async editFood() {
+//     onAuthStateChanged(auth, (user) => {
+//       thingsRef.add({
+//         uid: user.uid,
+//         name: faker.commerce.productName(),
+//         createdAt: serverTimestamp()
+//       });
+//     })
+//   }
+// };
