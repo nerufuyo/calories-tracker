4ce2208bf9d3d5644ec4474f0ac88258911e3c7f
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-18 01:08:18 +0700

    change import auth location

--- user/assets/scripts/utils/userLoginTest.js
+++ user/assets/scripts/utils/userLoginTest.js
@@ -1,5 +1,5 @@
 import {signInWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js';
-import {auth} from '../globals/firebaseInit.js';
+import {auth} from './firebaseInit.js';
 
 function userLoginTest() {
   signInWithEmailAndPassword(auth, 'idwimeianto@gmail.com', 'RahasiaDong');
