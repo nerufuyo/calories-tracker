8865772f32e637731ca3265227d0486d6366d3d2
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-18 09:17:27 +0700

    add FoodDiaryDb.js with add food function

--- user/assets/scripts/data/FoodDiaryDb.js
+++ user/assets/scripts/data/FoodDiaryDb.js
@@ -1,9 +1,28 @@
-// const FoodDiaryDb = {
-  // add( { name, serving_size, calories, category, date} )
+import {db} from '../utils/firebaseInit.js';
+import {addDoc, collection, serverTimestamp} from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js';
+import UserDb from './UserDb.js';
+
+const colRef = collection(db, 'foodDiaries');
+
+const FoodDiaryDb = {
+  async add({name, servingSize, calories, category, date}) {
+    const user = await UserDb.getUserAuth();
+
+    return await addDoc(colRef, {
+      name,
+      servingSize,
+      calories,
+      category,
+      date: new Date(date),
+      uid: user.uid,
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+    });
+  },
   // update( { name, serving_size, calories, category, date} )
   // delete(id)
   // getById(id)
   // getByDateRange( { startDate, endDate } )
-// }; 
+};
 
-// export default FoodDiaryDb;
+export default FoodDiaryDb;
