7dd4104650cc079e910076f0c813750f7421c604
Imam Firdaus Dwimeianto <idwimeianto@gmail.com>
2021-12-18 10:12:36 +0700

    fix input date class and remove placeholder

--- user/assets/scripts/views/pages/editFood/EditFoodView.js
+++ user/assets/scripts/views/pages/editFood/EditFoodView.js
@@ -13,20 +13,20 @@ class EditFoodView {
         <label>
           <div class="input-group food-name__input">
             <div class="input-name">Name</div>
-            <div class="form-input"><input type="text" placeholder="Enter food name" value="Spaghetti" class="food-name" required></div>
+            <div class="form-input"><input type="text" placeholder="Enter food name" class="food-name" required></div>
           </div>
         </label>
         <label>
           <div class="input-group serving-size__input">
             <div class="input-name">Serving Size</div>
-            <div class="form-input"><input type="number" placeholder="Enter food serving size" value="200" min="0" class="serving-size" required></div>
+            <div class="form-input"><input type="number" placeholder="Enter food serving size" min="0" class="serving-size" required></div>
             <div> / g</div>
           </div>
         </label>
         <label>
           <div class="input-group calories__input">
             <div class="input-name">Calories</div>
-            <div class="form-input"><input type="number" placeholder="Enter food calories" class="food-calories" value="300" min="0" required></div>
+            <div class="form-input"><input type="number" placeholder="Enter food calories" class="food-calories" min="0" required></div>
           </div>
         </label>
         <label>
@@ -34,7 +34,7 @@ class EditFoodView {
             <div class="input-name">Category</div>
             <div class="form-input">
               <select class="food-category">
-                <option value="Breakfast" selected>Breakfast</option>
+                <option value="Breakfast">Breakfast</option>
                 <option value="Lunch">Lunch</option>
                 <option value="Dinner">Dinner</option>
                 <option value="Snack">Snack</option>
@@ -45,7 +45,7 @@ class EditFoodView {
         <label>
           <div class="input-group date__input">
             <div class="input-name">Date</div>
-            <div class="form-input"><input type="date" class="food-calories" value="${DateHelper.getYYYYMMDD(new Date())}" required></div>
+            <div class="form-input"><input type="date" class="food-date" required></div>
           </div>
         </label>
         <div class="edit-food-button__container">
